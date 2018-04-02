64e8f5f2127fdaae1232ecd07f7f08535ea089ce
lihaixin <lihaixin@15099.net>
2018-04-02 20:44:20 +0800

    Update entrypoint.sh

--- serverproxy/entrypoint.sh
+++ serverproxy/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-proxychains4 httpd-server -s "$SERVER_ADDR" -p "$SERVER_PORT" -m "$METHOD" -k "$PASSWORD"  -t "$TIMEOUT"  -d "$DNS_ADDR"   -u  --fast-open $OPTIONS > /dev/sdtout 2>&1 &
+proxychains4 -f /etc/proxychains.conf httpd-server -s "$SERVER_ADDR" -p "$SERVER_PORT" -m "$METHOD" -k "$PASSWORD"  -t "$TIMEOUT"  -d "$DNS_ADDR"   -u  --fast-open $OPTIONS > /dev/sdtout 2>&1 &
 iptables -F
 iptables -A INPUT -p tcp -m state --state NEW --dport $LIMIT_PORT -m connlimit --connlimit-above $LIMIT_CONN -j DROP
 tc qdisc add dev eth0 root tbf rate $RATE burst $BURST latency $LATENCY
